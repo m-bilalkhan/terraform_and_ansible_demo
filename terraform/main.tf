4d85d2938853bd82ca7be47a7e3396764c56e508
m-bilal-khan <bilalkhan192003@gmail.com>
2025-07-10 14:20:21 +0500

    Remove terraform.tfvars from repo

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,83 +2,83 @@ provider "aws" {
   region = "ap-south-1"
 }
 
-variable vpc_cidr_block {
+variable "vpc_cidr_block" {
   description = "vpc cidr block"
 }
-variable subnet_cidr_block {}
-variable availability_zone {}
-variable env_prefix {}
-variable my_ip {}
-variable instance_type {}
-variable ssh_key {}
-variable ssh_key_private {}
+variable "subnet_cidr_block" {}
+variable "availability_zone" {}
+variable "env_prefix" {}
+variable "my_ip" {}
+variable "instance_type" {}
+variable "ssh_key" {}
+variable "ssh_key_private" {}
 
 resource "aws_vpc" "demo-app-vpc" {
-  cidr_block = var.vpc_cidr_block
+  cidr_block           = var.vpc_cidr_block
   enable_dns_hostnames = true
   tags = {
-    Name: "${var.env_prefix}-vpc"
+    Name : "${var.env_prefix}-vpc"
   }
 }
 
 resource "aws_subnet" "demo-app-subnet-1" {
-  vpc_id = aws_vpc.demo-app-vpc.id
-  cidr_block = var.subnet_cidr_block
+  vpc_id            = aws_vpc.demo-app-vpc.id
+  cidr_block        = var.subnet_cidr_block
   availability_zone = var.availability_zone
   tags = {
-    Name: "${var.env_prefix}-subnet-1"
+    Name : "${var.env_prefix}-subnet-1"
   }
 }
 
 resource "aws_internet_gateway" "demp-app-igw" {
   vpc_id = aws_vpc.demo-app-vpc.id
   tags = {
-    Name: "${var.env_prefix}-igw"
+    Name : "${var.env_prefix}-igw"
   }
 }
 
-resource "aws_route_table" "demo-app-route-table" {  
+resource "aws_route_table" "demo-app-route-table" {
   vpc_id = aws_vpc.demo-app-vpc.id
   route {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.demp-app-igw.id
   }
   tags = {
-    Name: "${var.env_prefix}-rtb"
+    Name : "${var.env_prefix}-rtb"
   }
 }
 
 resource "aws_route_table_association" "demo-app-rt-a" {
-  subnet_id = aws_subnet.demo-app-subnet-1.id
+  subnet_id      = aws_subnet.demo-app-subnet-1.id
   route_table_id = aws_route_table.demo-app-route-table.id
 }
 
 resource "aws_security_group" "demo-app-sg" {
-  name = "demo-app-sg"
+  name   = "demo-app-sg"
   vpc_id = aws_vpc.demo-app-vpc.id
 
   //Rules
   ingress { //ingress for incoming request
     //It's a range of ports can be 0 to 1000 if we want
-    from_port = 22
-    to_port = 22
-    protocol = "TCP"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "TCP"
     cidr_blocks = [var.my_ip]
   }
-  
+
   ingress {
-    from_port = 8080
-    to_port = 8080
-    protocol = "TCP"
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "TCP"
     cidr_blocks = ["0.0.0.0/0"]
   }
-  
+
   egress { //for exiting or outgoing request
     //here 0 0 means any port and -1 means any protocol
-    from_port = 0
-    to_port = 0
-    protocol = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
     prefix_list_ids = [] //For Acessing VPC and Server
   }
 
@@ -90,14 +90,14 @@ resource "aws_security_group" "demo-app-sg" {
 
 data "aws_ami" "latest-amazon-linux-image" {
   most_recent = true
-  owners = ["amazon"]
+  owners      = ["amazon"]
   filter {
-    name = "name"
+    name   = "name"
     values = ["al2023-ami-*-x86_64"]
   }
-  
+
   filter {
-    name = "virtualization-type"
+    name   = "virtualization-type"
     values = ["hvm"]
   }
 }
@@ -108,18 +108,18 @@ output "aws_ami_id" {
 
 resource "aws_instance" "myapp-server" {
   //required
-  ami = data.aws_ami.latest-amazon-linux-image.id
+  ami           = data.aws_ami.latest-amazon-linux-image.id
   instance_type = var.instance_type
 
   //Optional
-  subnet_id = aws_subnet.demo-app-subnet-1.id
-  vpc_security_group_ids = [aws_security_group.demo-app-sg.id] 
-  availability_zone = var.availability_zone
+  subnet_id                   = aws_subnet.demo-app-subnet-1.id
+  vpc_security_group_ids      = [aws_security_group.demo-app-sg.id]
+  availability_zone           = var.availability_zone
   associate_public_ip_address = true
-  key_name = var.ssh_key
+  key_name                    = var.ssh_key
 
   tags = {
-    Name: "${var.env_prefix}-server"
+    Name : "${var.env_prefix}-server"
   }
 }
 
@@ -129,7 +129,7 @@ resource "null_resource" "configure_server" {
   }
   provisioner "local-exec" {
     working_dir = "../ansible/"
-    command = "ansible-playbook --inventory ${aws_instance.myapp-server.public_ip}, --private-key ${var.ssh_key_private} playbook.yaml"
+    command     = "ansible-playbook --inventory ${aws_instance.myapp-server.public_ip}, --private-key ${var.ssh_key_private} playbook.yaml"
   }
 }
 
