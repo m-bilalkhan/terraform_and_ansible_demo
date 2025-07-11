b71a1b7ea7534241c442e00b8c9d45c93c01807e
m-bilal-khan <bilalkhan.192003@gmail.com>
2025-07-11 11:23:55 +0500

    heradoc bug fix

--- terraform/main.tf
+++ terraform/main.tf
@@ -135,7 +135,7 @@ resource "null_resource" "configure_server" {
     command = <<EOT
         echo "${var.ssh_key_private}" > temp_key.pem
         chmod 600 temp_key.pem
-        ansible-playbook --inventory '${aws_instance.myapp-server.public_ip},' --private-key temp_key.pem -u ubuntu playbook.yaml
+        ansible-playbook --inventory '${aws_instance.myapp-server.public_ip},' --private-key temp_key.pem -u ubuntu ./playbook.yaml
         rm temp_key.pem
       EOT
     }
